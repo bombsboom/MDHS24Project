c00528313fe749c7243c057a19c2eaeb39bc3e1f
348576356 <348576356@404X0164JTXGJH2.EDU.YRDSB.NET>
2024-05-17 16:03:38 -0400

    fd

--- src/io/github/bombsboom/protectionEvents.java
+++ src/io/github/bombsboom/protectionEvents.java
@@ -32,13 +32,23 @@ public class protectionEvents implements Listener {
 			if (source.getCausingEntity() instanceof Player) {
 				Player victim = (Player) e.getEntity();
 				Player attacker = (Player) source.getCausingEntity();
-
+				
 				if(!source.isIndirect()) {
 					Material item = attacker.getInventory().getItemInMainHand().getType();
 					if(item == Material.IRON_SWORD) {
 						victim.setHealth(0);
+						
 					}else{ // random junk or fist
 						e.setCancelled(true);
+					}
+				}else{
+					if(damage.DamageType(ARROW) && mainPlugin.playerRoles.getuniqueID){
+						victim.sethealth(0)
+					else{
+						attacker.sethealth(0)
+						attacker.itemdrop(BOW)
+					}
+						
 					}
 				}
 			}
