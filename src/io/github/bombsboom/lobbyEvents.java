d418e85cbdca895f7127c75b94ae64463360dbbf
Brandon Li <libr340849488@gmail.com>
2024-05-17 15:40:33 -0400

    added basic lobby logic

--- src/io/github/bombsboom/lobbyEvents.java
+++ src/io/github/bombsboom/lobbyEvents.java
@@ -1,14 +1,48 @@
 package io.github.bombsboom;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-
+import org.bukkit.event.player.PlayerQuitEvent;
 public class lobbyEvents implements Listener {
-
+	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent e) {
+		Player p = e.getPlayer();
+		mainPlugin.playerRoles.put(p.getUniqueId(),-1);
 		
+		if(mainPlugin.playerRoles.size() >= 5) {
+			GameCountDownEvent event = new GameCountDownEvent();
+			Bukkit.getPluginManager().callEvent(event);
+			
+		}
 	}
 	
+	@EventHandler
+	public void onPlayerLeave(PlayerQuitEvent e) {
+		Player p = e.getPlayer();
+		mainPlugin.playerRoles.remove(p);
+	}
+	
+	@EventHandler
+	public void onGameCountDown(GameCountDownEvent e) {
+		// role logic
+		Random rand = new Random();
+		
+		Object[] players = Bukkit.getOnlinePlayers().toArray();
+		
+		int next = rand.nextInt(players.length);
+		
+		do {
+			
+		} while()
+		
+	}
 }
--- src/io/github/bombsboom/mainPlugin.java
+++ src/io/github/bombsboom/mainPlugin.java
@@ -1,20 +1,29 @@
 package io.github.bombsboom;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class mainPlugin extends JavaPlugin{
+	
+	public static HashMap<UUID, Integer> playerRoles = new HashMap<>();
+	
+	
+	
 	@Override
 	public void onLoad() {
-		getServer().getLogger().info("[Murder Myster] Loaded");
+		getServer().getLogger().info("[Murder Mystery] Loaded");
+		
 	}
 	
 	@Override
 	public void onEnable() {
-		getServer().getLogger().info("[Murder Myster] Enabled");
+		getServer().getLogger().info("[Murder Mystery] Enabled");
 	}
 	
 	@Override
 	public void onDisable() {
-		getServer().getLogger().info("[Murder Myster] Disabled");
+		getServer().getLogger().info("[Murder Mystery] Disabled");
 	}
 }
